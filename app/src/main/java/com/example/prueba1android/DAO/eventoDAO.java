589298e9b9f307a225b8bbab0a79520d7b55d45b
Manuel Meza Martinez <59111498+Souldox@users.noreply.github.com>
2020-09-14 16:44:54 -0300

    DAO
    array de evento DTO

--- app/src/main/java/com/example/prueba1android/DAO/eventoDAO.java
+++ app/src/main/java/com/example/prueba1android/DAO/eventoDAO.java
@@ -1,6 +1,22 @@
 package com.example.prueba1android.DAO;
 
-public class eventoDAO {
+import android.content.Context;
+
+import com.example.prueba1android.DTO.eventoDTO;
+
+import java.util.ArrayList;
+
+public class eventoDAO extends eventoDTO {
+
+    private Context context;
+    private ArrayList<eventoDTO> listItem;
+
+    public eventoDAO(String nota, String genero, String artista, String fecha, int precio, Context context, ArrayList<eventoDTO> listItem) {
+        super(nota, genero, artista, fecha, precio);
+        this.context = context;
+        this.listItem = listItem;
+    }
+
 
 
 }
